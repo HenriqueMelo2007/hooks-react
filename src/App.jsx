d8f3687130706bbe2777191837f65fd7667b3171
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2023-05-22 17:06:56 -0300

    alterações diversas

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import { Box } from "./components/box"
 
 export function App() {
 
-  const hooks = ['useState', 'useEffect', 'useContext', 'useReducer', 'useCallback', 'useMemo', 'useRef', 'useImperativeHandle', 'useLayoutEffect', 'useDebugValue', 'useDeferredValue', 'useTransition', 'useId', 'useSyncExternalStore', 'useInsertionEffect']
+  const hooks = [ {hook: 'useState', info: 'Gerencia o estado interno de um componente funcional.', id: 1}, {hook: 'useEffect', info: 'Executa efeitos colaterais em resposta a mudanças no componente.', id: 2}, {hook: 'useContext', info: 'Acessa um contexto definido em um componente pai.', id: 3}, {hook: 'useReducer', info: 'Gerencia o estado complexo com base em ações.', id: 4}, {hook: 'useRef', info: 'Cria uma referência mutável que persiste entre re-renderizações.', id: 5}, {hook: 'useMemo', info: 'Memoriza o valor de retorno de uma função para evitar cálculos desnecessários.', id: 6}, {hook: 'useCallback', info: 'Memoriza uma versão otimizada de uma função para evitar re-renderizações desnecessárias.', id: 7}, {hook: 'useLayoutEffect', info: 'Executa um efeito síncrono após as mutações do DOM.', id: 8}, {hook: 'useImperativeHandle', info: 'Personaliza a instância exposta de um componente filho.', id: 9}, {hook: 'useDebugValue', info: 'Fornece informações de depuração personalizadas para um hook personalizado.', id: 10} ]
 
   return (
     <div>
 
-      {hooks.map( (hooks) => {
-        return <Box hook={hooks}></Box>
-      } )}
+      <div className="header">
+        <h1>React Hooks</h1>
+      </div>
+
+      <div className="container">
+        {hooks.map( ( { hook, info, id } ) => {
+        return <Box hook={hook} info={info} key={id}></Box>
+        } )}
+      </div>
+
+      
       
     </div>
   )
--- src/components/box/index.jsx
+++ src/components/box/index.jsx
@@ -1,11 +1,15 @@
 import style from './box.module.css'
 
-export function Box ({ hook }) {
+export function Box ({ hook, info }) {
+
   return (
 
     <div className={style.div}>
-      <h2 className={style.hookName}>{hook}</h2>
-      <input className={style.input} type="checkbox" />
+      <div className={style.header}>
+        <input className={style.input} type="checkbox" />
+        <h2 className={style.hook}>{hook}</h2>
+      </div>
+      <p className={style.info}>{info}</p>
     </div>
 
   )
